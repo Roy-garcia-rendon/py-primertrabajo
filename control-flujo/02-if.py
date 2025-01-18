dc2e95320fd6227c602494bb95920ad4589468ac
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-01-17 21:31:40 -0600

    se crea carpeta control de flujo

---
+++ control-flujo/02-if.py
@@ -0,0 +1,11 @@
+edad = input("Digite su edad: ")
+edad = int(edad)
+if edad >= 54:
+    print("puede ver la peli con descuento")
+elif edad > 17:
+    print("puede ver la peli")
+else:
+    print("no puede ver la peli")
+    print("vete a ver la de los pitufos")
+
+print("fin del programa")
