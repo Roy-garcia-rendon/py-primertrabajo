3ee21e4cd8d605ad933f543373c1639015492e4c
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-01-25 20:43:28 -0600

    archivo erroneo de prueba 05-alcance.py, variables globales

--- funciones/05-alcance.py
+++ funciones/05-alcance.py
@@ -1,9 +1,18 @@
+saludo = "hola global"
+
+
 def saludar():
+    global saludo
     saludo = "hola mundo"
 
 
 def saludarpero():
+    global saludo
     saludo = "hola nacos"
+    print(saludo)
 
 
 print(saludo)
+saludar()
+print(saludo)
+saludarpero()
