b9ccae3fcc7162d2405813d334453e067de2306f
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-01-27 21:44:04 -0600

    archivo de ejercicio de preactica palindromo

---
+++ ejercicios/palindromo.py
@@ -0,0 +1,14 @@
+def palindromo(texto):
+    texto = texto.remplace(" ", "")
+    texto = texto.lower()
+
+
+while True:
+
+    print("escriba una palabra para saber si es palindromo")
+    text = text.remplace(" ", "")
+    text = text.lower()
+    text = input("escriba una palabra:")
+    if text.lower() == "salir":
+        break
+    print(text)
