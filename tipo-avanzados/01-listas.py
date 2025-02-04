3c6338ab4ed010600dcc0529e92216474264c618
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-02-03 21:01:14 -0600

    listas, manupular listas

--- tipo-avanzados/01-listas.py
+++ tipo-avanzados/01-listas.py
@@ -1,1 +1,12 @@
 numeros = [1, 2, 3]
+letras = ['a', 'b', 'c']
+palabras = ['hola', 'mundo']
+palabrasfeliz = ['hola', 'mundo', "holaaa", "mundooo"]
+booleans = [True, False, True]
+matriz = [[0, 1], [2, 3], [4, 5]]
+ceros = [0] * 10
+cerosyuno = [0, 1] * 10
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("hola mundo")
+print(chars)
